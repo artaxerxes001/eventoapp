6ee50e85c5c5ceee366f7af6f6a87ec32ad4f01d
artaxerxes001 <artaxerxesnazare@gmail.com>
2021-10-28 18:00:30 +0100

    melhorando a aparecencia do loyout e mudando as cores

--- src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
@@ -17,7 +17,7 @@ public class DataConfiguration {
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?useSSL=true");
 		dataSource.setUsername("root");
-		dataSource.setPassword("password");
+		dataSource.setPassword("Capricornioesmeralda#1");
 		return dataSource;
 	}
 	
